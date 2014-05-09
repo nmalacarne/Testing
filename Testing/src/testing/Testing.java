3c369a6e1de562bc2e6c9485df840d2a272a5af8
nmalacarne <nicholas.malacarne@gmail.com>
2014-05-09 10:28:36 -0400

    - added and removed comments

--- Testing/src/testing/Testing.java
+++ Testing/src/testing/Testing.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package testing;
 
 /**
@@ -17,6 +11,7 @@ public class Testing {
      */
     public static void main(String[] args) {
         
+        // testing comment
     }
     
 }
